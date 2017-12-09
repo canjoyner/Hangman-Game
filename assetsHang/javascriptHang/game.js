7c6976743df078a0fc1ca2bf923e236e6a2359d0
Candace Joyner <candace.joyner>
2017-12-09 14:42:41 -0500

    with you win

--- assetsHang/javascriptHang/game.js
+++ assetsHang/javascriptHang/game.js
@@ -2,73 +2,79 @@
 var wordBank = ["The Matrix" ,"Lord of the Rings", "The Dark Crystal"];
 var gameTime = document.getElementById("newGame");
 var greeting = document.getElementById("welcome");
-var attemps = document.getElementById("guesses");	
+var attemps = document.getElementById("guesses");
+var picture = document.getElementById("result");	
 
 var numRand = Math.floor(Math.random() * wordBank.length);
 var wins = 0;  //not funtioning
 var current = [];
 var storeTemp =[];
-var guess =[];
+var guess =[];var lowerguess
 var storeGuess = [];
 var chosenWord = [];
 var display = [];
-var test = "r";
+var test = ["you win"];
 
 console.log(wins);
 console.log(wordBank)
 
 //click a key to begin
 
- document.onkeyup = function(){}
-console.log(current)
+ document.onkeyup = function(){
+// console.log(current)
 //picks a word from the list and converts it to dashes
     var chosenWord  = wordBank[numRand];
     console.log(chosenWord)
-  console.log("test")
-      for (var i=0; i < chosenWord.length; i++) {
-     storeTemp[i] = "-";   
+    // console.log("test")
+       // for (var i=0; i < chosenWord.length; i++) {
+       // storeTemp[i] = "-";   
+//        if (chosenWord.charAt(i) === " "){
+//      	    storeTemp[i] = " ";}
+//      	    current[i] = storeTemp[i];
+//      	    gameTime.innerHTML = current;
+//           // picture.innerHTML = "test"; 
+// }
+       // }
 
-          if (chosenWord.charAt(i) === " "){
+// console.log(storeTemp)
+// console.log(current)
 
-
-     	storeTemp[i] = " ";}
-     	current[i] = storeTemp[i];
-     	gameTime.innerHTML = current;
-     
- }
-
-console.log(storeTemp)
-console.log(current)
-console.log(chosenWord)
 // gathers key strokes. Code should have a loop to prevent users from guessing the same word twice
   onkeyup = function (event){ 
 
+    console.log(chosenWord)
+    var wordString = display.join("");
+    console.log(wordString)
 
- {
-    guess = event.key;
-    storeGuess.push(guess);
+   if (wordString != chosenWord) {
+    
+        guess = event.key;
+        storeGuess.push(guess);
+        // display = current;
 
+        var lowerGuess = guess.toLowerCase();
+        var lowerChosenWord = chosenWord.toLowerCase();
    
-	for (var j=0; j < chosenWord.length; j++){
-  if (chosenWord.charAt(j) != test) {
+	       for (var j=0; j < chosenWord.length; j++){
 
-  	// the  += notation here is adding to the entire string instead of 
-  	// keeping the string a set length
-  display += current[j];}
-   else {
-   display += chosenWord[j];
-   }   
-  
-  console.log(display)
-
-attemps.innerHTML = storeGuess;
-gameTime.innerHTML = display;
+            if (lowerChosenWord.charAt(j) != lowerGuess) {
+              	 display[j] = display[j];}
+            else {display[j] = chosenWord[j];
+                  }   
+            attemps.innerHTML = storeGuess;
+            gameTime.innerHTML = display;
+        }
+   console.log(display.length)
+   console.log(wordString.length)
+  }
 
 
-  
-
-
-};
+  else {
+       picture.innerHTML = test;
+      win += 1; 
+      
+}
+}
 }
    
 // there are several things missing from this code
@@ -79,7 +85,7 @@ gameTime.innerHTML = display;
 
 
 
-}
+
 
 
  
